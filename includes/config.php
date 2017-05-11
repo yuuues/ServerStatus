a9c77ad99228aa445c7c972da677b30425255619
Kevin Cala Sánchez <kevin@kcsystem.es>
2017-05-11 10:36:32 +0200

    Update

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,15 @@
 <?php
+    $host = 'localhost';
+    $user = '';
+    $pass = '';
+    $data = 'status';
+    $sSetting['refresh'] = "10000";
 
-$host = 'localhost';
-$user = '';
-$pass = '';
-$data = 'status';
-$sSetting['refresh'] = "10000";
+    mysql_connect($host, $user, $pass) or die(mysql_error());
+    mysql_select_db($data) or die(mysql_error());
 
-mysql_connect($host, $user, $pass) or die(mysql_error());
-mysql_select_db($data) or die(mysql_error());
-//Template options: "default" and "dark"
-$template = "./templates/default/";
-$index = $template . "index.php";
+    $template = "./templates/default/";
+    $index = $template."index.php";
 ?>
+
+mys
