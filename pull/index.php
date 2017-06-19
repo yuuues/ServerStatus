5eeaca52919daf001ef491426691eb4c8b3411fc
Kevin Cala Sánchez <kevin@kcsystem.es>
2017-06-19 15:37:31 +0200

    Moved Logic to host instead on remote

--- pull/index.php
+++ pull/index.php
@@ -38,7 +38,39 @@
             echo json_encode($array);
         } else {
             $data = json_decode($output, true);
+            if (0 == $memory = $data['memory']) {
+                $memlevel = null;
+            } elseif ($memory >= 51) {
+                $memlevel = "success";
+            } elseif ($memory <= 50) {
+                $memlevel = "warning";
+            } elseif ($memory <= 35) {
+                $memlevel = "danger";
+            }
+
+            $data['memory'] = (!is_null($memlevel)) ? '<div class="progress progress-striped active">
+<div class="bar bar-'.$memlevel.'" style="width: '.$memory.'%;">'.$memory.'%</div> </div>'
+                    : '<div class = "progress progress-striped active">
+            <div class = "bar bar-info" style = "width: 100%;">Unknown</div></div>';
+
+            if (0 == $hdd = $data['hdd']) {
+                $hddlevel = null;
+            } elseif ($hdd >= 51) {
+                $hddlevel = "success";
+            } elseif ($hdd <= 50) {
+                $hddlevel = "warning";
+            } elseif ($hdd <= 35) {
+                $hddlevel = "danger";
+            }
+            $data['hdd'] = (!is_null($hddlevel)) ? '<div class="progress progress-striped active">
+<div class="bar bar-'.$hddlevel.'" style="width: '.$hdd.'%;">'.$hdd.'%</div></div>'
+                    : '<div class = "progress progress-striped active">
+            <div class = "bar bar-info" style = "width: 100%;">Unknown</div></div>';
+
             $data["load"] = number_format($data["load"], 2);
+            $data['online'] = '<div class = "progress">
+            <div class = "bar bar-success" style = "width: 100%;"><small>Up</small></div>
+            </div>';
             echo json_encode($data);
         }
     }
